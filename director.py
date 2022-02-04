a61b3d8ac3166b552fd489b59f650861a05ea3e3
BrendonsDad <orraubury@gmail.com>
2022-02-03 20:44:27 -0700

    made jumper

---
+++ director.py
@@ -0,0 +1,24 @@
+from word import Word
+from display import Display
+from score import Score
+
+class Director:
+    
+    def __init__(self):
+        #stuff in there
+        
+        self.word = Word()
+        self.play = True
+        self.display = Display()
+
+    def start_game(self):
+        while self.play == True:
+            self.display
+            self.guess()
+
+    def guess():
+        guesstuff = 'stuff'
+
+    def display():
+        dis = 'display '
+
