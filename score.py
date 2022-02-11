28edd5276df05a1122e08aaa5c72dfa3f566ae95
Punyanith Tangsiriruangrit <punyanith.t@gmail.com>
2022-02-10 20:46:24 -0700

    score code total_score check_letter letter_index

--- score.py
+++ score.py
@@ -1,5 +1,42 @@
-class Score:
-    def __init__(self):
-        self._score = []
+from word import Word
 
-# work on score
+
+class Score:
+    def __init__(self,score,letter,word):
+        self._score = score
+        self.current_score = 5
+        self.sub_lives = -1
+        self.letter = letter
+        self.word = word
+        self.index = []
+
+    def total_score(self):
+        if self.check_letter():
+            return self.current_score
+        else:
+            return self.sub_lives + self.current_score 
+
+
+    def check_letter(self):
+        for i in self.word:
+           if i == self.letter:
+               return True
+        return False
+
+    def letter_index(self):
+        for i in range(len(self.word)):
+            if self.word[i] == self.letter:
+                self.index.append(i)
+        return self.index
+
+        # check a letter to see if its in word class
+        # pass in get the letter and the word
+        # which index of the letter
+        # guess is connected to score
+        # score is connected to display
+
+        # 5 lives
+
+        # if guess is correct the letter is revealed.
+
+        #if guess is incorrect, a line on a player parachute is cut.
